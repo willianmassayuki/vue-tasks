72b228d3350e624f2a83cc5ba266cb121a34cf8e
Willian Sato <willianmassayukisato@gmail.com>
2026-04-06 09:30:31 -0300

    add task store update, delete

--- src/store/tasksStore.ts
+++ src/store/tasksStore.ts
@@ -46,9 +46,60 @@ export const useTasksStore = defineStore("tasks", () => {
     }
   };
 
+  const updateTask = async (
+    id: number,
+    title: string,
+  ): Promise<Task | void> => {
+    if (!title.trim()) {
+      return;
+    }
+
+    loading.value = true;
+    error.value = null;
+
+    try {
+      const updateTask = await tasksService.update(id, {
+        title: title.trim(),
+      });
+
+      const index = tasks.value.findIndex((t) => t.id === id);
+
+      if (index !== -1) {
+        tasks.value[index] = updateTask;
+      }
+
+      return updateTask;
+    } catch (err) {
+      const axiosError = err as { response?: { data?: { message?: string } } };
+      error.value =
+        axiosError.response?.data?.message || "Erro ao atualizar a tarefa";
+      console.error("Erro ao atualizar a tarefa:", err);
+      throw err;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const deleteTask = async (id: number): Promise<void> => {
+    loading.value = true;
+    try {
+      await tasksService.delete(id);
+      tasks.value = tasks.value.filter((t) => t.id !== id);
+    } catch (err) {
+      const axiosError = err as { response?: { data?: { message?: string } } };
+      error.value =
+        axiosError.response?.data?.message || "Erro ao deletar a tarefa";
+      console.error("Erro ao deletar a tarefa:", err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
   return {
     loadTasks,
     createTask,
+    updateTask,
+    deleteTask,
     error,
     loading,
     tasks,
